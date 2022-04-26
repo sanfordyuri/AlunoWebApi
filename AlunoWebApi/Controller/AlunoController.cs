2d77f5af968d9eec5c0d06af75d50b3f119624fd
Yuri Sanford <yurisanford@fortestecnologia.com.br>
2022-04-26 11:18:56 -0300

    Primeiro video - Criação Entidade e Operação Post & Get

---
+++ AlunoWebApi/Controller/AlunoController.cs
@@ -0,0 +1,34 @@
+﻿using AlunoWebApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace AlunoWebApi.Controller
+{
+
+    [ApiController]
+    [Route("aluno")]
+    public class AlunoController : ControllerBase
+    {
+        public AlunoController()
+        {
+
+        }
+
+        public static List<Aluno> Alunos = new List<Aluno>(); //Representação de um Banco de dados.
+
+        [HttpPost]
+        public void AdicionarAluno([FromBody] Aluno aluno)
+        {
+            Alunos.Add(aluno);
+            Console.WriteLine("Novo aluno adicionado: " + aluno.Nome);
+        }
+
+        [HttpGet]
+        public IEnumerable<Aluno> RetonarAlunos()
+        {
+            return Alunos;
+        }
+
+    }
+}
---
+++ AlunoWebApi/Model/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlunoWebApi.Model
+{
+    public class Aluno
+    {
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime Nascimento { get; set; }
+
+    }
+}
